513abc840b7ac92e4f8a4a7ecab2964007bf25f5
Alexandre Dulaunoy <a@foo.be>
2019-02-23 14:26:19 +0100

    chg: [svg export] ensuring fonts are converted to path

--- tools/convert-svg.sh
+++ tools/convert-svg.sh
@@ -5,10 +5,9 @@ createsvg() {
   local svg
   for d in ../ai/*.ai; do
     svg=$(echo "$d" | sed 's/.ai/.svg/')
-    echo "creating $svg ..."
     output=$(echo "$svg" | sed 's/...//' | sed 's/\.ai/.svg/')
-    echo "$output"
-    inkscape -f "$d" -l "../$output"
+    echo "Convert AI to SVG $output ..."
+    inkscape --export-text-to-path -f "$d" -l "../$output"
   done
 }
 
